1ad982bbba9d574586dd656cf2604dc6d0921d14
deepakagarwalAIS <45588840+deepakagarwalAIS@users.noreply.github.com>
2018-12-07 19:23:24 +0530

    Add files via upload
    File path and name are hardcoded and we need a existing directory to download the blob

---
+++ StorageBlobUploadDownload.ps1
@@ -0,0 +1,19 @@
+﻿#Login-AzureRmAccount
+$resourceGroup = "testResourceGroup"
+$storageAccountName = "teststorageaaccount"
+
+$storageAccount = Get-AzureRmStorageAccount | where {$_.StorageAccountName -eq "teststorageaaccount"}
+
+$key1 = (Get-AzureRmStorageAccountKey -ResourceGroupName $storageAccount.ResourceGroupName -Name $storageAccount.StorageAccountName).GetValue(0)
+
+$storageContext = New-AzureStorageContext -StorageAccountName $storageAccount.StorageAccountName -StorageAccountKey $key1.Value
+
+
+$fileName = "test.jpg"
+$filePath = "C:\Temp\" + $fileName
+
+Set-AzureStorageBlobContent -Container "demo"  -File $filePath -Blob $fileName -Force -BlobType Block -Context $storageContext
+
+$destPath = "C:\Downloads\"
+
+Get-AzureStorageBlobContent -Blob $fileName -Container "demo" -Destination $destPath -Context $storageContext
